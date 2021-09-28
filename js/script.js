5d99864a2a5306cf7988060e839ceb330712a437
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-09-28 17:12:38 -0600

    Added pokemonList array

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    heightInMeters: 0.7,
+    type: ['Grass', 'Poison']
+  },
+  {
+    name: 'Charmander',
+    heightInMeters: 0.6,
+    type: 'Fire'
+  },
+  {
+    name: 'Squirtle',
+    heightInMeters: 0.5,
+    type: 'Water'
+  }
+]
+
+console.log(pokemonList);
