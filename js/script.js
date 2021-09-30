f5f6be0a3513503a2a7d1729e3589d05ecf6b725
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-09-30 11:59:11 -0600

    Replaced for loop with forEach

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    heightInMeters: 0.7,
-    type: ['Grass', 'Poison']
-  },
-  {
-    name: 'Charizard',
-    heightInMeters: 1.7,
-    type: 'Fire'
-  },
-  {
-    name: 'Squirtle',
-    heightInMeters: 0.5,
-    type: 'Water'
-  }
+	{
+		name: 'Bulbasaur',
+		heightInMeters: 0.7,
+		type: ['Grass', 'Poison'],
+	},
+	{
+		name: 'Charizard',
+		heightInMeters: 1.7,
+		type: 'Fire',
+	},
+	{
+		name: 'Squirtle',
+		heightInMeters: 0.5,
+		type: 'Water',
+	},
 ];
 
 // Prints Pokemon name and height in the DOM
-for(let i = 0; i < pokemonList.length; i++) {
-  let pokemonName = pokemonList[i].name
-  let pokemonHeight = pokemonList[i].heightInMeters
+pokemonList.forEach(pokemon => {
+	let { name, heightInMeters } = pokemon;
+  printPokemon(name, heightInMeters)
+});
 
-  if (pokemonHeight > 1) {
-    document.write('</br>')
-    document.write(`${pokemonName}: height(${pokemonHeight}) - Wow, that's big! `)
-    document.write('</br>')
-  } else {
-    document.write('</br>')
-    document.write(`${pokemonName}: height(${pokemonHeight}) `);
-    document.write('</br>')
-  }
+function printPokemon(name, heightInMeters) {
+	document.write(
+		heightInMeters > 1
+			? ` </br> ${name}: height(${heightInMeters}) - Wow, that's big! </br>`
+			: ` </br> ${name}: height(${heightInMeters}) </br>`
+	);
 }
-
-
-
 
