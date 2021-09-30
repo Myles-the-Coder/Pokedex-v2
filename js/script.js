eb1fd92169ea71a1961ec0147003757be48ca3b5
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-09-30 12:57:33 -0600

    Created IIFE with utility functions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,60 @@
-let pokemonList = [
-	{
-		name: 'Bulbasaur',
-		heightInMeters: 0.7,
-		type: ['Grass', 'Poison'],
-	},
-	{
-		name: 'Charizard',
-		heightInMeters: 1.7,
-		type: 'Fire',
-	},
-	{
-		name: 'Squirtle',
-		heightInMeters: 0.5,
-		type: 'Water',
-	},
-];
+
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      heightInMeters: 0.7,
+      type: ['Grass', 'Poison'],
+    },
+    {
+      name: 'Charizard',
+      heightInMeters: 1.7,
+      type: 'Fire',
+    },
+    {
+      name: 'Squirtle',
+      heightInMeters: 0.5,
+      type: 'Water',
+    },
+  ];
+
+  function add(pokemon) {
+    function areValidKeys() {
+      if (Object.keys(pokemon).includes('name') &&
+      Object.keys(pokemon).includes('heightInMeters') &&
+      Object.keys(pokemon).includes('type')) {
+        return true
+      }
+    }
+
+    if(typeof pokemon === 'object' && areValidKeys()) {
+      return pokemonList.push(pokemon)
+    };
+  }
+
+  function getAll() {
+    return pokemonList
+  }
+
+  function filterPokemon(name) {
+    return pokemonList.filter(pokemon => pokemon.name === name)
+  }
+
+  return {
+    add,
+    getAll,
+    filterPokemon
+  }
+})()
+
+pokemonRepository.add({
+  name: 'Pikachu',
+  heightInMeters: 0.4,
+  type: 'Electric'
+});
 
 // Prints Pokemon name and height in the DOM
-pokemonList.forEach(pokemon => {
+pokemonRepository.getAll().forEach(pokemon => {
 	let { name, heightInMeters } = pokemon;
   printPokemon(name, heightInMeters)
 });
