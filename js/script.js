fb7e1aff26e3d36d94fdf505a43caf31d9dbfb1d
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2022-01-02 16:48:21 -0700

    Refactoring and converted project to SCSS

--- js/script.js
+++ js/script.js
@@ -6,17 +6,31 @@ let pokemonRepository = (function () {
 	const modal = document.querySelector('#poke-modal');
 	const modalBody = document.querySelector('.modal-body');
 	let pokeApi = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+  const colors = {
+    fire: "#FDDFDF",
+    grass: "#DEFDE0",
+    electric: "#FCF7DE",
+    water: "#DEF3FD",
+    ground: "#f4e7da",
+    rock: "#d5d5d4",
+    fairy: "#fceaff",
+    poison: "#98d7a5",
+    bug: "#f8d5a3",
+    dragon: "#97b3e6",
+    psychic: "#eaeda1",
+    flying: "#778899",
+    fighting: "#E6E0D4",
+    normal: "#F5F5F5",
+  };
+  const main_types = Object.keys(colors);
 
-	function add(pokemon) {
-		if (typeof pokemon === 'object') {
-			return pokemonList.push(pokemon);
-		}
-	}
+	const add = pokemon => {
+		if (typeof pokemon === 'object') return pokemonList.push(pokemon);
+	};
 
-	const getAll = () => pokemonList
-
-	const filterPokemon = name => pokemonList.filter(pokemon => pokemon.name === name);
-
+	const getAll = () => pokemonList;
+	const filterPokemon = name =>
+		pokemonList.filter(pokemon => pokemon.name === name);
 	const capitalize = word => word.toUpperCase().slice(0, 1) + word.slice(1);
 
 	function addListItem(pokemon) {
@@ -38,6 +52,7 @@ let pokemonRepository = (function () {
 	//Display Pokemon Info in modal
 	function showDetails(pokemon) {
 		loadDetails(pokemon).then(() => {
+      const {name, imageUrl, types} = pokemon
 			const modalHeader = document.querySelector('.modal-header');
 			const modalTitle = document.querySelector('.modal-title');
 			const modalBody = document.querySelector('.modal-body');
@@ -47,8 +62,14 @@ let pokemonRepository = (function () {
 			const pokemonName = document.createElement('h2');
 			const closeModalBtn = document.createElement('button');
 
-			pokemonImg.src = `${pokemon.imageUrl}`;
-			modalTitle.innerText = `${capitalize(pokemon.name)}`;
+      const poke_types = types.map(type => type.type.name);
+      const type = capitalize(main_types.find(type => poke_types.indexOf(type) > -1))
+      const color = colors[type[0].toLowerCase() + type.slice(1)];
+
+      modalBody.style.backgroundColor = color
+
+			pokemonImg.src = `${imageUrl}`;
+			modalTitle.innerText = `${capitalize(name)}`;
 			closeModalBtn.innerText = 'X';
 			closeModalBtn.setAttribute('data-bs-dismiss', 'modal');
 
@@ -85,8 +106,9 @@ let pokemonRepository = (function () {
 		}
 	}
 
-	const hideLoadingIcon = () => loadingIcon.forEach(icon => icon.classList.add('hidden'));
-  
+	const hideLoadingIcon = () =>
+		loadingIcon.forEach(icon => icon.classList.add('hidden'));
+
 	function handleError(err) {
 		hideLoadingIcon();
 		console.error(err);
@@ -97,6 +119,7 @@ let pokemonRepository = (function () {
 		try {
 			const res = await fetch(pokeApi);
 			const data = await res.json();
+      console.log(data)
 			data.results.forEach(item => {
 				let pokemon = {
 					name: item.name,
@@ -122,24 +145,21 @@ let pokemonRepository = (function () {
 		}
 	}
 
-	function filterPokemonList(e) {
+	const filterPokemonList = e => {
 		e.preventDefault();
 		let pokeList = document.querySelectorAll('.group-list-item');
 		let searchTerm = searchInput.value.toLowerCase();
-		if (searchTerm) {
-			pokeList.forEach(pokemon => {
-				pokemon.innerText.toLowerCase().indexOf(searchTerm) > -1
-					? pokemon.classList.remove('hidden')
-					: pokemon.classList.add('hidden');
-			});
-		}
-
-		searchTerm === '' &&
-			pokeList.forEach(pokemon => pokemon.classList.remove('hidden'));
-	}
+		searchTerm !== ''
+			? pokeList.forEach(pokemon => {
+					pokemon.innerText.toLowerCase().indexOf(searchTerm) > -1
+						? pokemon.classList.remove('hidden')
+						: pokemon.classList.add('hidden');
+			  })
+			: pokeList.forEach(pokemon => pokemon.classList.remove('hidden'));
+	};
 
 	searchInput.addEventListener('input', e => filterPokemonList(e));
-	modal.addEventListener('hidden.bs.modal', () => modalBody.innerHTML = '');
+	modal.addEventListener('hidden.bs.modal', () => (modalBody.innerHTML = ''));
 
 	return {
 		getAll,
